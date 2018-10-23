313282290952a1c11f45c8042bacbe097189d287
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-10-23 10:58:31 +0530

    Delete everything. Because I want to start again

--- test/run_all_test.sh
+++
@@ -1,6 +0,0 @@
-../run_test.sh ../src/pattern_main.js all_rectangle_input all_rectangle_output
-../run_test.sh ../src/pattern_main.js all_triangle_input all_triangle_output
-../run_test.sh ../src/pattern_main.js all_diamond_input all_diamond_output
-node all_rectangle_test.js
-node all_triangle_test.js
-node all_diamond_test.js
