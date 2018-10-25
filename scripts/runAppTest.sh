d1b973defc6e33f5112f23e96741a03fb0148262
Shubham Chauhan <44020728+shubhiamchauhan@users.noreply.github.com>
2018-10-25 14:31:50 +0530

    rename run_all_test and moved all Ip and Op files
    to runAppTest

---
+++ scripts/runAppTest.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+./scripts/run_test.sh generateRectangle.js ./appTestData/allPatternInput/allRectangleInput ./appTestData/allPatternOutput/allRectangleOutput #rectangle
+./scripts/run_test.sh generateTriangle.js ./appTestData/allPatternInput/allTriangleInput ./appTestData/allPatternOutput/allTriangleOutput #triangle
+./scripts/run_test.sh generateDiamond.js ./appTestData/allPatternInput/allDiamondInput ./appTestData/allPatternOutput/allDiamondOutput #diamond
--- scripts/run_all_test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-./run_test.sh rectangle_main.js ./test/all_pattern_input/all_rectangle_input ./test/all_pattern_output/all_rectangle_output #rectangle
-./run_test.sh triangle_main.js ./test/all_pattern_input/all_triangle_input ./test/all_pattern_output/all_triangle_output #triangle
-./run_test.sh diamond_main.js ./test/all_pattern_input/all_diamond_input ./test/all_pattern_output/all_diamond_output #diamond
